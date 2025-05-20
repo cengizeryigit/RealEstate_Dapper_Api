0d0f86f263da9ea67c49a90ae08640815b1a2eaf
cengiz eryiğit <cengiz_eryigit@hotmail.com>
2025-05-20 21:46:35 +0300

    İstatistik İşlemleri

---
+++ RealEstate_Dapper_Api/Controllers/StatisticsController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using RealEstate_Dapper_Api.Repositories.StatisticsRepositories;
+
+namespace RealEstate_Dapper_Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StatisticsController : ControllerBase
+    {
+        private readonly IStatisticsRepository _statisticsRepository;
+
+        public StatisticsController(IStatisticsRepository statisticsRepository)
+        {
+            _statisticsRepository = statisticsRepository;
+        }
+
+        [HttpGet("ActiveCategoryCount")]
+        public IActionResult ActiveCategoryCount()
+        {
+            var result = _statisticsRepository.ActiveCategoryCount();
+            return Ok(result);
+        }
+
+        [HttpGet("ActiveEmployeeCount")]
+        public IActionResult ActiveEmployeeCount()
+        {
+            var result = _statisticsRepository.ActiveEmployeeCount();
+            return Ok(result);
+        }
+    }
+}
