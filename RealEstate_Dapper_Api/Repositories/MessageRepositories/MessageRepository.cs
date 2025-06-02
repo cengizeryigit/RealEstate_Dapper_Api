820f3e57026ff616b79155cc8613ec7d98ded1cc
cengiz eryiğit <cengiz_eryigit@hotmail.com>
2025-06-02 20:20:00 +0300

    Estate Agent Navbar Son 3 Mesajı UI'da Listeleme

--- RealEstate_Dapper_Api/Repositories/MessageRepositories/MessageRepository.cs
+++ RealEstate_Dapper_Api/Repositories/MessageRepositories/MessageRepository.cs
@@ -15,7 +15,7 @@ namespace RealEstate_Dapper_Api.Repositories.MessageRepositories
 
         public async Task<List<ResultInboxMessageDto>> GetInBoxLast3MessageListByReceiver(int id)
         {
-            string query = "select top(3)* from Message Where Receiver = @receiverid order by MessageId desc ";
+            string query = "select top(3) MessageId,UserName as 'Name' ,UserImageUrl,Subject,Detail,SendDate, IsRead from Message inner join AppUser On Message.Sender=AppUser.UserID Where Receiver = 2 order by MessageId desc ";
             var parameters = new DynamicParameters();
             parameters.Add("receiverid", id);
             using (var connection = _context.CreateConnection())
--- RealEstate_Dapper_UI/Dtos/MessageDtos/ResultInBoxMessageDto.cs
+++ RealEstate_Dapper_UI/Dtos/MessageDtos/ResultInBoxMessageDto.cs
@@ -1,6 +1,6 @@
-﻿namespace RealEstate_Dapper_Api.Dtos.MessageDtos
+﻿namespace RealEstate_Dapper_UI.Dtos.MessageDtos
 {
-    public class ResultInboxMessageDto
+    public class ResultInBoxMessageDto
     {
         public int MessageId { get; set; }
         public string Name { get; set; }
@@ -8,7 +8,6 @@
         public string Detail { get; set; }
         public DateTime SendDate { get; set; }
         public bool IsRead { get; set; }
-
         public string UserImageUrl { get; set; }
     }
 }
