c7791fa6edfc3927d4ed22876e056fc241bc0529
cengiz eryiğit <cengiz_eryigit@hotmail.com>
2025-06-02 15:26:16 +0300

    Giriş Yapan Emlakçıya Ait Dashboard İstatistikler

---
+++ RealEstate_Dapper_Api/Controllers/EstateAgentDashboardStatisticController.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using RealEstate_Dapper_Api.Repositories.EstateAgentRepositories.DashboardRepositories.StatisticRepositories;
+
+
+namespace RealEstate_Dapper_Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EstateAgentDashboardStatisticController : ControllerBase
+    {
+        private readonly IStatisticRepository _statisticsRepository;
+
+        public EstateAgentDashboardStatisticController(IStatisticRepository statisticsRepository)
+        {
+            _statisticsRepository = statisticsRepository;
+        }
+
+        [HttpGet("AllProductCount")]
+        public IActionResult AllProductCount()
+        {
+            var result = _statisticsRepository.AllProductCount();
+            return Ok(result);
+        }
+
+        [HttpGet("ProductCountByEmployeeId")]
+        public IActionResult ProductCountByEmployeeId(int id)
+        {
+            var result = _statisticsRepository.ProductCountByEmployeeId(id);
+            return Ok(result);
+        }
+
+        [HttpGet("ProductCountByStatusFalse")]
+        public IActionResult ProductCountByStatusFalse(int id)
+        {
+            var result = _statisticsRepository.ProductCountByStatusFalse(id);
+            return Ok(result);
+        }
+
+        [HttpGet("ProductCountByStatusTrue")]
+        public IActionResult ProductCountByStatusTrue(int id)
+        {
+            var result = _statisticsRepository.ProductCountByStatusTrue(id);
+            return Ok(result);
+        }
+    }
+}
---
+++ RealEstate_Dapper_UI/ViewComponents/EstateAgent/_EstateAgentDashboardStatisticComponentPartial.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace RealEstate_Dapper_UI.ViewComponents.EstateAgent
+{
+    public class _EstateAgentDashboardStatisticComponentPartial : ViewComponent
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public _EstateAgentDashboardStatisticComponentPartial(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var client = _httpClientFactory.CreateClient();
+
+            #region ProductCount - Toplam Ilan Sayisi     
+            var responseMessageProductCount = await client.GetAsync("https://localhost:44379/api/EstateAgentDashboardStatistic/AllProductCount");
+            var jsonDataProductCount = await responseMessageProductCount.Content.ReadAsStringAsync();
+            ViewBag.ProductCount = jsonDataProductCount;
+            #endregion
+
+            #region EmployeeByProductCount  - Eklakçının Toplam Ilan Sayisi
+            var responseMessageEmployeeByProductCount = await client.GetAsync("https://localhost:44379/api/EstateAgentDashboardStatistic/ProductCountByEmployeeId?id=1");
+            var jsonDataEmployeeByProductCount = await responseMessageEmployeeByProductCount.Content.ReadAsStringAsync();
+            ViewBag.EmployeeByProductCount = jsonDataEmployeeByProductCount;
+            #endregion
+
+            #region ProductCountByEmployeeByStatusTrue  - Eklakçının Aktif Ilan Sayisi
+            var responseMessageProductCountByEmployeeByStatusTrue = await client.GetAsync("https://localhost:44379/api/EstateAgentDashboardStatistic/ProductCountByStatusTrue?id=1");
+            var jsonDataProductCountByEmployeeByStatusTrue = await responseMessageProductCountByEmployeeByStatusTrue.Content.ReadAsStringAsync();
+            ViewBag.ProductCountByEmployeeByStatusTrue = jsonDataProductCountByEmployeeByStatusTrue;
+            #endregion
+
+            #region ProductCountByEmployeeByStatusFalse  - Eklakçının Pasif Ilan Sayisi
+            var responseMessageProductCountByEmployeeByStatusFalse = await client.GetAsync("https://localhost:44379/api/EstateAgentDashboardStatistic/ProductCountByStatusFalse?id=1");
+            var jsonDataProductCountByEmployeeByStatusFalse = await responseMessageProductCountByEmployeeByStatusFalse.Content.ReadAsStringAsync();
+            ViewBag.ProductCountByEmployeeByStatusFalse = jsonDataProductCountByEmployeeByStatusFalse;
+            #endregion
+
+            return View();
+        }
+    }
+}
