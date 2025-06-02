bdc6a424ada25d97f475651360255cf0d14cf555
cengiz eryiğit <cengiz_eryigit@hotmail.com>
2025-06-02 16:10:27 +0300

    Giriş Yapan Emlakçıya Özgü Dashboard Chart Api

---
+++ RealEstate_Dapper_UI/ViewComponents/EstateAgent/_EstateAgentDashboardChartComponentPartial.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace RealEstate_Dapper_UI.ViewComponents.EstateAgent
+{
+    public class _EstateAgentDashboardChartComponentPartial:ViewComponent
+    {
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            return View();
+        }
+    }
+    
+}
--- RealEstate_Dapper_UI/ViewComponents/EstateAgent/_EstateAgentDashboardStatisticComponentPartial.cs
+++ RealEstate_Dapper_UI/ViewComponents/EstateAgent/_EstateAgentDashboardStatisticComponentPartial.cs
@@ -1,17 +1,22 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using RealEstate_Dapper_UI.Services;
 
 namespace RealEstate_Dapper_UI.ViewComponents.EstateAgent
 {
     public class _EstateAgentDashboardStatisticComponentPartial : ViewComponent
     {
         private readonly IHttpClientFactory _httpClientFactory;
+        private readonly ILoginService _loginService;
 
-        public _EstateAgentDashboardStatisticComponentPartial(IHttpClientFactory httpClientFactory)
+        public _EstateAgentDashboardStatisticComponentPartial(IHttpClientFactory httpClientFactory, ILoginService loginService)
         {
             _httpClientFactory = httpClientFactory;
+            _loginService = loginService;
         }
+
         public async Task<IViewComponentResult> InvokeAsync()
         {
+            var id = _loginService.GetUserId;
             var client = _httpClientFactory.CreateClient();
 
             #region ProductCount - Toplam Ilan Sayisi     
@@ -21,19 +26,19 @@ namespace RealEstate_Dapper_UI.ViewComponents.EstateAgent
             #endregion
 
             #region EmployeeByProductCount  - Eklakçının Toplam Ilan Sayisi
-            var responseMessageEmployeeByProductCount = await client.GetAsync("https://localhost:44379/api/EstateAgentDashboardStatistic/ProductCountByEmployeeId?id=1");
+            var responseMessageEmployeeByProductCount = await client.GetAsync("https://localhost:44379/api/EstateAgentDashboardStatistic/ProductCountByEmployeeId?id="+id);
             var jsonDataEmployeeByProductCount = await responseMessageEmployeeByProductCount.Content.ReadAsStringAsync();
             ViewBag.EmployeeByProductCount = jsonDataEmployeeByProductCount;
             #endregion
 
             #region ProductCountByEmployeeByStatusTrue  - Eklakçının Aktif Ilan Sayisi
-            var responseMessageProductCountByEmployeeByStatusTrue = await client.GetAsync("https://localhost:44379/api/EstateAgentDashboardStatistic/ProductCountByStatusTrue?id=1");
+            var responseMessageProductCountByEmployeeByStatusTrue = await client.GetAsync("https://localhost:44379/api/EstateAgentDashboardStatistic/ProductCountByStatusTrue?id="+id);
             var jsonDataProductCountByEmployeeByStatusTrue = await responseMessageProductCountByEmployeeByStatusTrue.Content.ReadAsStringAsync();
             ViewBag.ProductCountByEmployeeByStatusTrue = jsonDataProductCountByEmployeeByStatusTrue;
             #endregion
 
             #region ProductCountByEmployeeByStatusFalse  - Eklakçının Pasif Ilan Sayisi
-            var responseMessageProductCountByEmployeeByStatusFalse = await client.GetAsync("https://localhost:44379/api/EstateAgentDashboardStatistic/ProductCountByStatusFalse?id=1");
+            var responseMessageProductCountByEmployeeByStatusFalse = await client.GetAsync("https://localhost:44379/api/EstateAgentDashboardStatistic/ProductCountByStatusFalse?id="+id);
             var jsonDataProductCountByEmployeeByStatusFalse = await responseMessageProductCountByEmployeeByStatusFalse.Content.ReadAsStringAsync();
             ViewBag.ProductCountByEmployeeByStatusFalse = jsonDataProductCountByEmployeeByStatusFalse;
             #endregion
