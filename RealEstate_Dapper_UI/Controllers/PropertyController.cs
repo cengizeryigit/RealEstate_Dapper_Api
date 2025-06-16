fe4dae04439d2b18fbf45d157534654645984f44
cengiz eryiğit <cengiz_eryigit@hotmail.com>
2025-06-16 16:12:32 +0300

    Property Single Sayfa Verileri

--- RealEstate_Dapper_UI/Controllers/PropertyController.cs
+++ RealEstate_Dapper_UI/Controllers/PropertyController.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
+using RealEstate_Dapper_UI.Dtos.ProductDetailDtos;
 using RealEstate_Dapper_UI.Dtos.ProductDtos;
+using System.Dynamic;
 
 namespace RealEstate_Dapper_UI.Controllers
 {
@@ -30,7 +32,22 @@ namespace RealEstate_Dapper_UI.Controllers
         [HttpGet]
         public async Task<IActionResult> PropertySingle(int id)
         {
-            return View();
+            dynamic model = new ExpandoObject();
+
+            id = 1;
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("https://localhost:44379/api/Products/GetProductByProductId?id=" + id);
+            var responseMessage2 = await client.GetAsync("https://localhost:44379/api/ProductDetails/GetProductDetailByProductId?id=" + id);
+            var jsonData = await responseMessage.Content.ReadAsStringAsync();
+            var jsonData2 = await responseMessage2.Content.ReadAsStringAsync();
+
+            model.values = JsonConvert.DeserializeObject<ResultProductDto>(jsonData);
+            model.details = JsonConvert.DeserializeObject<GetProductDetailByIdDto>(jsonData2);
+            // model.values.date = DateTime.Now.ToString("dd/MM/yyyy");
+
+
+            return View(model);
+
         }
     }
 }
