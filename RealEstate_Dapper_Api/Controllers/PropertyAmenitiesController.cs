ff63c783052b47ce611aa91c9af9be40339c72b6
cengiz eryiğit <cengiz_eryigit@hotmail.com>
2025-06-17 13:47:59 +0300

    Property Amenities Api Kısmı

---
+++ RealEstate_Dapper_Api/Controllers/PropertyAmenitiesController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using RealEstate_Dapper_Api.Repositories.PropertyAmenityRepositories;
+
+namespace RealEstate_Dapper_Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PropertyAmenitiesController : ControllerBase
+    {
+        private readonly IPropertyAmenityRepository _propertyAmenityRepository;
+
+        public PropertyAmenitiesController(IPropertyAmenityRepository propertyAmenityRepository)
+        {
+            _propertyAmenityRepository = propertyAmenityRepository;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetPropertyAmenityByStatusTrue(int id)
+        {
+            var result = await _propertyAmenityRepository.GetPropertyAmenityByStatusTrue(id);
+            if (result == null || !result.Any())
+            {
+                return NotFound("No amenities found for the specified property.");
+            }
+            return Ok(result);
+        }
+    }
+}
---
+++ RealEstate_Dapper_Api/Repositories/PropertyAmenityRepositories/PropertyAmenityRepository.cs
@@ -0,0 +1,27 @@
+﻿using Dapper;
+using RealEstate_Dapper_Api.Dtos.PropertyAmenityDtos;
+using RealEstate_Dapper_Api.Models.DapperContext;
+
+namespace RealEstate_Dapper_Api.Repositories.PropertyAmenityRepositories
+{
+    public class PropertyAmenityRepository : IPropertyAmenityRepository
+    {
+        private readonly Context _context;
+
+        public PropertyAmenityRepository(Context context)
+        {
+            _context = context;
+        }
+        public async Task<List<ResultPropertAmenityByStatusTrueDto>> GetPropertyAmenityByStatusTrue(int id)
+        {
+            string query = "select PropertyAmenityId,Title from PropertyAmenity inner join Amenity on PropertyAmenity.AmenityId = Amenity.AmenityId Where PropertyId = @propertyId And Status=1";
+            var parameters = new DynamicParameters();
+            parameters.Add("@propertyId", id);
+            using (var connection = _context.CreateConnection())
+            {
+                var values = await connection.QueryAsync<ResultPropertAmenityByStatusTrueDto>(query, parameters);
+                return values.ToList();
+            }
+        }
+    }
+}
