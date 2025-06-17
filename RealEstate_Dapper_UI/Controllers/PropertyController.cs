0c167a2f527aa0d1e0869be23e69b901ec02f02d
cengiz eryiğit <cengiz_eryigit@hotmail.com>
2025-06-17 14:05:35 +0300

    Amenity Consume ve Map Video Embed İşlemleri

--- RealEstate_Dapper_UI/Controllers/PropertyController.cs
+++ RealEstate_Dapper_UI/Controllers/PropertyController.cs
@@ -4,6 +4,7 @@ using RealEstate_Dapper_UI.Dtos.AppUserDtos;
 using RealEstate_Dapper_UI.Dtos.ProductDetailDtos;
 using RealEstate_Dapper_UI.Dtos.ProductDtos;
 using RealEstate_Dapper_UI.Dtos.ProductImageDtos;
+using RealEstate_Dapper_UI.Dtos.PropertyAmenityDtos;
 using System.Dynamic;
 
 namespace RealEstate_Dapper_UI.Controllers
@@ -42,16 +43,19 @@ namespace RealEstate_Dapper_UI.Controllers
             var responseMessageDetails = await client.GetAsync("https://localhost:44379/api/ProductDetails/GetProductDetailByProductId?id=" + id);
             var responseMessageImages = await client.GetAsync("https://localhost:44379/api/ProductImages/GetProductImageByProductId?productId=" + id);
             var responseMessageUser = await client.GetAsync("https://localhost:44379/api/AppUsers/GetAppUserByProductId?id=" + id);
+            var responseMessageAmenity = await client.GetAsync("https://localhost:44379/api/PropertyAmenities?id=" + id);
 
             var jsonDataValues = await responseMessageValues.Content.ReadAsStringAsync();
             var jsonDataDetails = await responseMessageDetails.Content.ReadAsStringAsync();
             var jsonDataImages = await responseMessageImages.Content.ReadAsStringAsync();
             var jsonDataUser = await responseMessageUser.Content.ReadAsStringAsync();
+            var jsonDataAmenity = await responseMessageAmenity.Content.ReadAsStringAsync();
 
             model.values = JsonConvert.DeserializeObject<ResultProductDto>(jsonDataValues);
             model.details = JsonConvert.DeserializeObject<GetProductDetailByIdDto>(jsonDataDetails);
             model.images = JsonConvert.DeserializeObject<List<GetProductImageByProductIdDto>>(jsonDataImages);
             model.user = JsonConvert.DeserializeObject<GetAppUserByProductByIdDto>(jsonDataUser);
+            model.amenity = JsonConvert.DeserializeObject<List<ResultPropertAmenityByStatusTrueDto>>(jsonDataAmenity);
 
 
             return View(model);
